2b329993bb54bedc18b43ea5c6724da24b07bab9
Richie <whitedogg13@gmail.com>
2020-02-05 10:06:53 +0800

    test on both platforms

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import NfcAppIndexV2 from 'react-native-nfc-manager/example/AppIndexV2'
-import NfcAppV2Apdu from 'react-native-nfc-manager/example/AppV2Apdu'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => NfcAppV2Apdu);
+AppRegistry.registerComponent(appName, () => NfcAppIndexV2);
